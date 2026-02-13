107d6bf5431878ed77be28fbed782a2d583b664d
Aderemi Alo <aloaderemi1@gmail.com>
2026-02-13 01:50:10 +0100

    changed a few things

--- lib/features/valentine/data/valentine_content.dart
+++ lib/features/valentine/data/valentine_content.dart
@@ -3,7 +3,12 @@ import '../domain/story_section.dart';
 const List<StorySection> kStorySections = [
   StorySection(
     title: 'You, In All Your Colors',
-    imageUrls: ['IMG_4188.jpg', 'IMG_4190.jpg', 'IMG_4211.jpg', 'IMG_4239.jpg'],
+    imageUrls: [
+      'images/IMG_4188.jpg',
+      'images/IMG_4190.jpg',
+      'images/IMG_4211.jpg',
+      'images/IMG_4239.jpg',
+    ],
     note:
         'You’re like a rainbow, a myriad of different colors. You’re not one thing, but many blended into one person, showing different sides at any given moment and I love it so much. \n \n You have so many varied interests, talking about the history of politics and how their decisions affect us greatly today, and then tomorrow you’re talking about your favorite Webtoon you’re currently reading. (Shout out to Flatmates with Benefits and remarried Empress).  \n \nYou are a multifaceted baddie, my queen and I wouldn’t change that about you for anything.',
     loveNotePlaceholder:
@@ -11,7 +16,12 @@ const List<StorySection> kStorySections = [
   ),
   StorySection(
     title: 'The Day I Knew',
-    imageUrls: ['IMG_4311.jpg', 'IMG_4260.jpg', 'IMG_4253.jpg', 'IMG_5559.JPG'],
+    imageUrls: [
+      'images/IMG_4311.jpg',
+      'images/IMG_4260.jpg',
+      'images/IMG_4253.jpg',
+      'images/IMG_5559.JPG',
+    ],
     note:
         'I’ve repeated this endlessly, but I can never forget the day I knew you were special like no other. We had met and talked before. I had seen you from afar and told myself you were extremely beautiful, and now even more so. \n \nBut that day it was your laugh, you were talking about how you wanted to marry an Arab man so they could spend money on you, and all I could see was you smile, joy and laughter and there and then, I knew I wanted to spend the rest of my life with you. \n\nIt may have taken me many more months to ask you out, but from that day there was no going back for me.',
     loveNotePlaceholder:
@@ -19,7 +29,12 @@ const List<StorySection> kStorySections = [
   ),
   StorySection(
     title: 'Our Best Moments',
-    imageUrls: ['image_1.JPG', 'IMG_6239.jpg', 'IMG_6248.jpg', 'image_2.JPG'],
+    imageUrls: [
+      'images/image_1.JPG',
+      'images/IMG_6239.jpg',
+      'images/IMG_6248.jpg',
+      'images/image_2.JPG',
+    ],
     note:
         'In the span of 3 years (that’s actually a lot), picking our best moments has been hard to do cause they’ve been so many despite being constantly attacked by long distance. \n\nBut I have to say, the moments that always come to mind, aren’t the flashiest, or when we go out. For me, it ’s when it’s just us two together with no one else around, cuddled up and doing what we do. \n\nWatching a movie, sleeping, or even eating a meal. Just being in your presence and being able to forget about the world around me, is always such a wonderful things to experience.',
     loveNotePlaceholder:
@@ -27,7 +42,12 @@ const List<StorySection> kStorySections = [
   ),
   StorySection(
     title: 'Still Choosing You',
-    imageUrls: ['IMG_6686.JPG', 'IMG_7299.jpg', 'IMG_7569.jpg', 'IMG_7601.jpg'],
+    imageUrls: [
+      'images/IMG_6686.JPG',
+      'images/IMG_7299.jpg',
+      'images/IMG_7569.jpg',
+      'images/IMG_7601.jpg',
+    ],
     note:
         'Every single day, I would continue to choose you, and only you. Over and over again for the rest of my life, you will always be my first choice and that would never change. I know you may argue about it sometimes, but honestly I didn’t choose this. \n\nI fell hard and fell deep in love with you, and I don’t want to be ever pulled out of it. So yes, I will choose you for the rest of our lives, over everything and everyone, cause you are my dream come true and my wishes answered.',
     loveNotePlaceholder:
@@ -36,13 +56,13 @@ const List<StorySection> kStorySections = [
 ];
 
 const List<String> kContentsCompanionImageUrls = [
-  'IMG_3160.jpg',
-  'IMG_3162.jpg',
-  'IMG_3184.jpg',
-  'IMG_3697.jpg',
+  'images/IMG_3160.jpg',
+  'images/IMG_3162.jpg',
+  'images/IMG_3184.jpg',
+  'images/IMG_3697.jpg',
 ];
 
-const String kLetterImageUrl = 'IMG_6323.JPG';
+const String kLetterImageUrl = 'images/IMG_6323.JPG';
 
 const String kLetterBody =
     'On this day dedicated to love, I write to you, to let you know where my heart lies. It rests with you, wherever you may go, whatever you may do. It follows you on the air, land and sea as you make your travels. It beats according to your moods always seeking to alleviate your sadness and elevate your joy. My heart belongs to you and only you my moonlight. \n\nI love you more than I can ever say and more than I can ever show, but I will always continue to try. I pray that this conveys a little of what I feel for you my love. \n\nI love you darling \nTo the moon and back \nAlways and forever. \n\nHappy Valentines day my sweetheart.';
